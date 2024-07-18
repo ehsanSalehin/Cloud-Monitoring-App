cffeaa17f5f671dc0b06e9819fabd5b3634a427d
EHSAN SALEHIN <106507122+ehsanSalehin@users.noreply.github.com>
2024-07-17 23:44:35 -0400

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -1,8 +1,8 @@
 import boto3
 
 region = 'ca-central-1'
-access_key_id = 'it's a secret :)'
-secret_access_key = 'it's a secret :)'
+access_key_id = 'secret :)'
+secret_access_key = 'secret :)'
 
 ecr_client = boto3.client('ecr',region_name=region, aws_access_key_id=access_key_id,
                           aws_secret_access_key=secret_access_key,)
